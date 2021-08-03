577308c768a56c8bf6ba64eaed644d06aa94842d
mcxiaoke <mcxiaoke@live.com>
2021-08-03 22:29:44 +0800

    media cli rename and move function tweak

--- scripts/media_cli.js
+++ scripts/media_cli.js
@@ -1,34 +1,46 @@
 #!/usr/bin/env node
+const dayjs = require("dayjs");
+const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs-extra");
 const chalk = require("chalk");
-const d = require("../lib/debug");
+const log = require("../lib/debug");
 const exif = require("../lib/exif");
+const helper = require("../lib/helper");
+// debug and logging config
+const prettyError = require("pretty-error").start();
+prettyError.skipNodeFiles();
+
+const configCli = (argv) => {
+  // log.setName("AudioCli");
+  log.setLevel(argv.verbose);
+  log.debug(argv);
+};
 
 const yargs = require("yargs/yargs")(process.argv.slice(2));
 // https://github.com/yargs/yargs/blob/master/docs/advanced.md
-const argv = yargs
-  .usage("Usage: $0 <command> <source> [options]")
+yargs
+  .usage("Usage: $0 <command> <input> [options]")
+  .positional("input", {
+    describe: "Input folder that contains files",
+    type: "string",
+    normalize: true,
+  })
   .command(
-    ["rename <source> [options]", "rn", "$0"],
-    "Rename media files in source dir by exif date",
+    ["rename <input> [options]", "rn", "$0"],
+    "Rename media files in input dir by exif date",
     (yargs) => {
       yargs
-        .positional("source", {
-          describe: "Source folder that contains media files",
-          type: "string",
-        })
         .option("backup", {
           alias: "b",
           type: "boolean",
           default: false,
           description: "backup original file before rename",
         })
-        .option("force", {
+        .option("fast", {
           alias: "f",
           type: "boolean",
-          default: false,
-          description: "force rename files without valid exif date",
+          description: "fast mode (use file modified time, no exif parse)",
         })
         .option("prefix", {
           alias: "p",
@@ -51,28 +63,244 @@ const argv = yargs
         });
     },
     (argv) => {
-      commandRename(argv);
+      cmdRename(argv);
+    }
+  )
+  .command(
+    ["organize <input>", "oz"],
+    "Organize pictures by file modified date",
+    (yargs) => {
+      yargs.option("output", {
+        alias: "o",
+        type: "string",
+        normalize: true,
+        description: "Output folder",
+      });
+    },
+    (argv) => {
+      cmdOrganize(argv);
     }
   )
   .count("verbose")
   .alias("v", "verbose")
   .alias("h", "help")
-  .alias("i", "version")
   .epilog(
-    "<Media Utilities>\nRename Image/Raw/Video files by EXIF date tags\nCopyright 2021 @ Zhang Xiaoke"
+    "Media Utilities: Rename Image/Raw/Video files by EXIF date tags\nCopyright 2021 @ Zhang Xiaoke"
   )
-  .demandCommand()
+  .demandCommand(1, chalk.red("Missing sub command you want to execute!"))
   .showHelpOnFail()
-  .help().argv;
-d.setLevel(argv.verbose);
-d.D(argv);
+  .help()
+  .middleware([configCli]);
+const argv = yargs.argv;
 
-async function commandRename(argv) {
-  const root = path.resolve(argv.source);
-  if (!root || !fs.pathExistsSync(root)) {
+async function renameFiles(files) {
+  log.info("Rename", `total ${files.length} files`);
+  // do rename all files
+  return await Promise.all(
+    files.map(async (f) => {
+      const outPath = path.join(path.dirname(f.path), f.outName);
+      try {
+        await fs.rename(f.path, outPath);
+        log.show(chalk.green(`Renamed:`) + ` ${outPath}`);
+        return f;
+      } catch (error) {
+        log.error("Rename", error, f.path);
+      }
+    })
+  );
+}
+
+async function cmdRename(argv) {
+  const root = path.resolve(argv.input);
+  if (!root || !(await fs.pathExists(root))) {
     yargs.showHelp();
-    d.E(chalk.red(`ERROR! Source '${root}' is not exists or not a directory!`));
+    log.error(`Invalid Input: '${root}'`);
     return;
   }
-  await exif.executeRename(root);
+  const fastMode = argv.fast || false;
+  // action: rename media file by exif date
+  const startMs = Date.now();
+  log.show("Rename", `Input: ${root}`, fastMode ? "(FastMode)" : "");
+  let files = await exif.listMedia(root);
+  const filesCount = files.length;
+  log.show("Rename", `Total ${files.length} media files found`);
+  files = await exif.parseFiles(files, { fastMode: fastMode });
+  log.show(
+    "Rename",
+    `Total ${files.length} media files parsed`,
+    fastMode ? "(FastMode)" : ""
+  );
+  files = exif.buildNames(files);
+  const [validFiles, skippedBySize, skippedByDate] = exif.checkFiles(files);
+  files = validFiles;
+  if (filesCount - files.length > 0) {
+    log.warn(
+      "Rename",
+      `Total ${filesCount - files.length} media files skipped`
+    );
+  }
+  log.show(
+    "Rename",
+    `Total ${filesCount} files processed in ${helper.humanTime(startMs)}`,
+    fastMode ? "(FastMode)" : ""
+  );
+  if (skippedBySize.length > 0) {
+    log.showYellow(
+      "Rename",
+      `Total ${skippedBySize.length} media files are skipped by size`
+    );
+  }
+  if (skippedByDate.length > 0) {
+    log.showYellow(
+      "Rename",
+      `Total ${skippedByDate.length} media files are skipped by date`
+    );
+  }
+  if (files.length == 0) {
+    log.showYellow("Rename", "Nothing to do, exit now.");
+    return;
+  }
+  log.show(
+    "Rename",
+    `Total ${files.length} media files ready to rename`,
+    fastMode ? "(FastMode)" : ""
+  );
+  const answer = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "yes",
+      default: false,
+      message: chalk.bold.red(
+        `Are you sure to rename ${files.length} files?` +
+          (fastMode ? " (FastMode)" : "")
+      ),
+    },
+  ]);
+  if (answer.yes) {
+    renameFiles(files).then((files) => {
+      log.showGreen("Rename", `There ${files.length} file were renamed.`);
+    });
+  } else {
+    log.showYellow("Rename", "Will do nothing, aborted by user.");
+  }
+}
+
+async function cmdOrganize(argv) {
+  const root = path.resolve(argv.input);
+  if (!root || !(await fs.pathExists(root))) {
+    yargs.showHelp();
+    log.error("Organize", `Invalid Input: '${root}'`);
+    return;
+  }
+  const output = argv.output || root;
+  // rules:
+  // 1. into folders by file type (png/video/image)
+  // 2. into folders by date month
+  log.show(`Organize: input:`, root);
+  log.show(`Organize: output:`, output);
+  let files = await exif.listMedia(root);
+  for (const f of files) {
+    if (!f.stats.isFile()) log.show("not file", f.path);
+  }
+  log.show("Organize", `Total ${files.length} media files found`);
+  pics = {};
+  files.forEach((f, i) => {
+    log.debug(`Processing(${i}):`, path.basename(f.path), f.stats.mtime);
+    if (helper.isVideoFile(f.path)) {
+      if (!pics["vids"]) {
+        pics["vids"] = [];
+      }
+      pics["vids"].push(f);
+      log.debug("Video Item:", f.path);
+    } else if ([".png", ".gif"].includes(helper.pathExt(f.path, true))) {
+      if (!pics["pngs"]) {
+        pics["pngs"] = [];
+      }
+      pics["pngs"].push(f);
+      log.debug("PNG Item:", f.path);
+    } else if (
+      f.stats.size < 1000 * 1024 &&
+      helper.pathExt(f.path, true) === ".jpg"
+    ) {
+      if (!pics["pngs"]) {
+        pics["pngs"] = [];
+      }
+      pics["pngs"].push(f);
+      log.debug("Other Item:", f.path, helper.fileSize(f.stats.size));
+    } else {
+      const dateStr = dayjs(f.stats.mtime).format("YYYYMM");
+      if (!pics[dateStr]) {
+        pics[dateStr] = [];
+      }
+      pics[dateStr].push(f);
+      log.debug("Image Item:", f.path, dateStr);
+      if (dateStr.includes("08")) {
+        log.show(f.path, f.stats.mtime);
+      }
+    }
+  });
+  for (const [k, v] of Object.entries(pics)) {
+    if (v.length > 0) {
+      log.show(
+        `Organize:`,
+        `${v.length} files will be moved to '${path.join(output, k)}'`
+      );
+    }
+  }
+  const answer = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "yes",
+      default: false,
+      message: chalk.bold.red(
+        `Are you sure to move these ${files.length} files?`
+      ),
+    },
+  ]);
+  if (answer.yes) {
+    for (const [k, v] of Object.entries(pics)) {
+      if (v.length > 0) {
+        const movedFiles = [];
+        const fileOutput = path.join(output, k);
+        for (const f of v) {
+          const fileSrc = f.path;
+          const fileDst = path.join(fileOutput, path.basename(fileSrc));
+          if (!(await fs.pathExists(fileSrc))) {
+            log.info("Not Found:", fileSrc);
+            continue;
+          }
+          if (await fs.pathExists(fileDst)) {
+            log.info("Skip Exists:", fileDst);
+            continue;
+          }
+          if (!(await fs.pathExists(fileOutput))) {
+            await fs.mkdirp(fileOutput);
+          }
+          try {
+            await fs.move(fileSrc, fileDst);
+            movedFiles.push([fileSrc, fileDst]);
+            log.info("Moved:", fileSrc, "to", fileDst);
+          } catch (error) {
+            log.error("Failed:", error, fileSrc, "to", fileDst);
+          }
+        }
+        if (v.length - movedFiles.length > 0) {
+          log.showYellow(
+            `Skipped:`,
+            `${
+              v.length - movedFiles.length
+            } files are already in '${fileOutput}'`
+          );
+        }
+        if (movedFiles.length > 0) {
+          log.showGreen(
+            `Done:`,
+            `${movedFiles.length} files are moved to '${fileOutput}'`
+          );
+        }
+      }
+    }
+  } else {
+    log.showYellow("Will do nothing, aborted by user.");
+  }
 }
