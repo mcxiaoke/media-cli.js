7cefa9ec180810622d56b8e79f87d139de09ea85
mcxiaoke <mcxiaoke@live.com>
2023-11-13 20:39:47 +0800

    use walk from fdir

--- scripts/test.js
+++ scripts/test.js
@@ -1,9 +1,23 @@
-const path = require("path");
-const fs = require("fs-extra");
+import path from "path";
+import fs from 'fs-extra';
+import { fdir } from "fdir";
+import { walk as __walk } from "@nodelib/fs.walk";
+import * as log from '../lib/debug.js'
+import * as exif from '../lib/exif.js'
+import * as helper from '../lib/helper.js'
+import * as mf from '../lib/file.js'
+import * as tools from '../lib/tools.js'
+import * as unicode from '../lib/unicode.js'
 
 async function main() {
-  console.log(process.argv);
-  await fs.ensureDir(process.argv.slice(2)[0]);
+  // console.log(process.argv);
+  const root = path.resolve(process.argv[2]);
+  console.log(root);
+  let files = await mf.walk(root);
+  console.log(files.length);
+  for (const f of files) {
+    // console.log(f);
+  }
 }
 
 main();
