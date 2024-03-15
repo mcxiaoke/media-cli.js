6b15a58d5459c0ff2b4d929cde93a2cb209090eb
mcxiaoke <mcxiaoke@live.com>
2024-03-15 16:29:41 +0800

    move cmd prefix to standalone dir, refact1

---
+++ cmd/cmd_prefix.js
@@ -0,0 +1,135 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import path from "path";
+import fs from 'fs-extra';
+import chalk from 'chalk';
+import yargs from "yargs";
+
+import { renameFiles } from "../lib/functions.js";
+
+import * as log from '../lib/debug.js'
+import * as helper from '../lib/helper.js'
+import * as mf from '../lib/file.js'
+
+export function cmdPrefixBuilder(ya) {
+    ya.option("size", {
+        alias: "s",
+        type: "number",
+        default: 24,
+        description: "size[length] of prefix of dir name",
+    })
+        .option("ignore", {
+            alias: "i",
+            type: "string",
+            description: "ignore string of prefix of dir name",
+        })
+        .option("prefix", {
+            alias: "p",
+            type: "string",
+            description: "filename prefix for output ",
+        })
+        .option("all", {
+            alias: "a",
+            type: "boolean",
+            description: "force rename all files ",
+        })
+}
+
+export async function cmdPrefix(argv) {
+    log.show('cmdPrefix', argv);
+    const root = path.resolve(argv.input);
+    if (!root || !(await fs.pathExists(root))) {
+        yargs.showHelp();
+        log.error(`Invalid Input: '${root}'`);
+        return;
+    }
+    const fastMode = argv.fast || false;
+    const allMode = argv.all || false;
+    const startMs = Date.now();
+    log.show("Prefix", `Input: ${root}`, fastMode ? "(FastMode)" : "");
+    let files = await mf.walk(root, {
+        entryFilter: (entry) =>
+            entry.stats.isFile() &&
+            entry.stats.size > 1024
+    });
+    // process only image files
+    // files = files.filter(x => helper.isImageFile(x.path));
+    files.sort();
+    log.show("Prefix", `Total ${files.length} files found`);
+    if (files.length == 0) {
+        log.showYellow("Prefix", "Nothing to do, exit now.");
+        return;
+    }
+    //let nameIndex = 0;
+    // 正则：仅包含数字
+    const reOnlyNum = /^\d+$/;
+    // 正则：匹配除 中日韩俄英 之外的特殊字符
+    const reNonChars = /[^\p{sc=Hani}\p{sc=Hira}\p{sc=Kana}\p{sc=Hang}\p{sc=Cyrl}\w_]/ugi;
+    const tasks = [];
+    for (const f of files) {
+        const [dir, base, ext] = helper.pathSplit(f.path);
+        if (!reOnlyNum.test(base) && !allMode) {
+            log.showYellow("Prefix", `Ignore: ${helper.pathShort(f.path)}`);
+            continue;
+        }
+        // 取目录项的最后两级目录名
+        let dirFix = dir.split(path.sep).slice(-2).join("_");
+        // 去掉目录名中的年月日
+        let dirStr = dirFix.replaceAll(/\d{4}-\d{2}-\d{2}/gi, "");
+        dirStr = dirStr.replaceAll(/\d+年\d+月/gi, "");
+        // 去掉附加说明
+        dirStr = dirStr.replaceAll(/\[.+\]/gi, "");
+        dirStr = dirStr.replaceAll(/\(.+\)/gi, "");
+        dirStr = dirStr.replaceAll(/\d+P(\d+V)?/gi, "");
+        // 去掉所有特殊字符
+        dirStr = dirStr.replaceAll(reNonChars, "");
+        if (argv.ignore && argv.ignore.length >= 2) {
+            dirStr = dirStr.replaceAll(argv.ignore, "");
+        } else {
+            dirStr = dirStr.replaceAll(/更新|合集|画师|图片|视频|插画|视图|订阅|限定|差分|R18|PSD|PIXIV|PIC|NO|ZIP|RAR/gi, "");
+        }
+        const nameSlice = (argv.size || 24) * -1;
+        // 去掉所有特殊字符
+        let oldBase = base.replaceAll(reNonChars, "");
+        //oldBase = oldBase.replaceAll(/\s/gi, "").slice(nameSlice);
+        const fPrefix = (dirStr + "_" + oldBase).slice(nameSlice);
+        const newName = `${fPrefix}${ext}`;
+        const newPath = path.join(dir, newName);
+        f.outName = newName;
+        log.show("Prefix", `Output: ${helper.pathShort(newPath)}`);
+        tasks.push(f);
+    }
+    if (tasks.length > 0) {
+        log.showGreen(
+            "Prefix",
+            `Total ${files.length} media files ready to rename`,
+            allMode ? "(allMode)" : ""
+        );
+    } else {
+        log.showYellow(
+            "Prefix",
+            `Nothing to do, abort.`,
+            allMode ? "(allMode)" : ""
+        );
+        return;
+    }
+
+    const answer = await inquirer.prompt([
+        {
+            type: "confirm",
+            name: "yes",
+            default: false,
+            message: chalk.bold.red(
+                `Are you sure to rename ${tasks.length} files?` +
+                (allMode ? " (allMode)" : "")
+            ),
+        },
+    ]);
+    if (answer.yes) {
+        renameFiles(tasks).then((tasks) => {
+            log.showGreen("Prefix", `There ${tasks.length} file were renamed.`);
+        });
+    } else {
+        log.showYellow("Prefix", "Will do nothing, aborted by user.");
+    }
+}
--- scripts/media_cli.js
+++ scripts/media_cli.js
@@ -11,7 +11,8 @@ import chalk from 'chalk';
 import yargs from "yargs";
 import PrettyError from 'pretty-error';
 import { cpus, tmpdir } from "os";
-
+import { renameFiles } from "../lib/functions.js";
+import { cmdPrefix, cmdPrefixBuilder } from "../cmd/cmd_prefix.js";
 import * as log from '../lib/debug.js'
 import * as exif from '../lib/exif.js'
 import * as helper from '../lib/helper.js'
@@ -326,29 +327,7 @@ ya
   .command(
     ["prefix <input> [output]", "px"],
     "Rename files by append dir name or fixed string",
-    (ya) => {
-      ya.option("size", {
-        alias: "s",
-        type: "number",
-        default: 24,
-        description: "size[length] of prefix of dir name",
-      })
-        .option("ignore", {
-          alias: "i",
-          type: "string",
-          description: "ignore string of prefix of dir name",
-        })
-        .option("prefix", {
-          alias: "p",
-          type: "string",
-          description: "filename prefix for output ",
-        })
-        .option("all", {
-          alias: "a",
-          type: "boolean",
-          description: "force rename all files ",
-        })
-    },
+    (ya) => cmdPrefixBuilder(ya),
     (argv) => {
       cmdPrefix(argv);
     }
@@ -365,137 +344,7 @@ ya
   .middleware([configCli]);
 const yargv = ya.argv;
 
-// 这个函数是一个异步函数，用于重命名文件  
-async function renameFiles(files) {
-  // 打印日志信息，显示要重命名的文件总数  
-  log.info("Rename", `total ${files.length} files`);
 
-  // 使用 Promise.all 方法异步处理所有文件  
-  // rename all files  
-  return await Promise.all(
-    files.map(async (f) => {
-      // 生成输出文件的路径  
-      const outPath = path.join(path.dirname(f.path), f.outName);
-
-      // 如果输出文件名不存在或者输入文件路径等于输出文件路径，忽略该文件并打印警告信息  
-      if (!f.outName || f.path == f.outPath) {
-        log.showYellow("Rename", "ignore", f.path);
-        return;
-      }
-
-      try {
-        // 使用 fs 模块的 rename 方法重命名文件，并等待操作完成  
-        await fs.rename(f.path, outPath);
-
-        // 打印重命名成功的日志信息，显示输出文件的路径  
-        log.show(chalk.green(`Renamed:`) + ` ${outPath}`);
-        return f;
-      } catch (error) {
-        // 捕获并打印重命名过程中出现的错误信息，显示错误原因和输入文件的路径  
-        log.error("Rename", error, f.path);
-      }
-    })
-  );
-}
-
-async function cmdPrefix(argv) {
-  log.show('cmdPrefix', argv);
-  const root = path.resolve(argv.input);
-  if (!root || !(await fs.pathExists(root))) {
-    yargs.showHelp();
-    log.error(`Invalid Input: '${root}'`);
-    return;
-  }
-  const fastMode = argv.fast || false;
-  const allMode = argv.all || false;
-  const startMs = Date.now();
-  log.show("Prefix", `Input: ${root}`, fastMode ? "(FastMode)" : "");
-  let files = await mf.walk(root, {
-    entryFilter: (entry) =>
-      entry.stats.isFile() &&
-      entry.stats.size > 1024
-  });
-  // process only image files
-  // files = files.filter(x => helper.isImageFile(x.path));
-  files.sort();
-  log.show("Prefix", `Total ${files.length} files found`);
-  if (files.length == 0) {
-    log.showYellow("Prefix", "Nothing to do, exit now.");
-    return;
-  }
-  //let nameIndex = 0;
-  // 正则：仅包含数字
-  const reOnlyNum = /^\d+$/;
-  // 正则：匹配除 中日韩俄英 之外的特殊字符
-  const reNonChars = /[^\p{sc=Hani}\p{sc=Hira}\p{sc=Kana}\p{sc=Hang}\p{sc=Cyrl}\w_]/ugi;
-  const tasks = [];
-  for (const f of files) {
-    const [dir, base, ext] = helper.pathSplit(f.path);
-    if (!reOnlyNum.test(base) && !allMode) {
-      log.showYellow("Prefix", `Ignore: ${helper.pathShort(f.path)}`);
-      continue;
-    }
-    // 取目录项的最后两级目录名
-    let dirFix = dir.split(path.sep).slice(-2).join("_");
-    // 去掉目录名中的年月日
-    let dirStr = dirFix.replaceAll(/\d{4}-\d{2}-\d{2}/gi, "");
-    dirStr = dirStr.replaceAll(/\d+年\d+月/gi, "");
-    // 去掉附加说明
-    dirStr = dirStr.replaceAll(/\[.+\]/gi, "");
-    dirStr = dirStr.replaceAll(/\(.+\)/gi, "");
-    dirStr = dirStr.replaceAll(/\d+P(\d+V)?/gi, "");
-    // 去掉所有特殊字符
-    dirStr = dirStr.replaceAll(reNonChars, "");
-    if (argv.ignore && argv.ignore.length >= 2) {
-      dirStr = dirStr.replaceAll(argv.ignore, "");
-    } else {
-      dirStr = dirStr.replaceAll(/更新|合集|画师|图片|视频|插画|视图|订阅|限定|差分|R18|PSD|PIXIV|PIC|NO|ZIP|RAR/gi, "");
-    }
-    const nameSlice = (argv.size || 24) * -1;
-    // 去掉所有特殊字符
-    let oldBase = base.replaceAll(reNonChars, "");
-    //oldBase = oldBase.replaceAll(/\s/gi, "").slice(nameSlice);
-    const fPrefix = (dirStr + "_" + oldBase).slice(nameSlice);
-    const newName = `${fPrefix}${ext}`;
-    const newPath = path.join(dir, newName);
-    f.outName = newName;
-    log.show("Prefix", `Output: ${helper.pathShort(newPath)}`);
-    tasks.push(f);
-  }
-  if (tasks.length > 0) {
-    log.showGreen(
-      "Prefix",
-      `Total ${files.length} media files ready to rename`,
-      allMode ? "(allMode)" : ""
-    );
-  } else {
-    log.showYellow(
-      "Prefix",
-      `Nothing to do, abort.`,
-      allMode ? "(allMode)" : ""
-    );
-    return;
-  }
-
-  const answer = await inquirer.prompt([
-    {
-      type: "confirm",
-      name: "yes",
-      default: false,
-      message: chalk.bold.red(
-        `Are you sure to rename ${tasks.length} files?` +
-        (allMode ? " (allMode)" : "")
-      ),
-    },
-  ]);
-  if (answer.yes) {
-    renameFiles(tasks).then((tasks) => {
-      log.showGreen("Prefix", `There ${tasks.length} file were renamed.`);
-    });
-  } else {
-    log.showYellow("Prefix", "Will do nothing, aborted by user.");
-  }
-}
 
 async function cmdRename(argv) {
   log.show('cmdRename', argv);
