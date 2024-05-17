e4c93a1fff25d11a987702727c84ae46f500a99e
mcxiaoke <mcxiaoke@live.com>
2024-05-17 12:43:20 +0800

    add rm selected files

---
+++ labs/rm_selected_files.js
@@ -0,0 +1,67 @@
+/*
+ * Project: mediac
+ * Created: 2024-05-16 17:25:25
+ * Modified: 2024-05-16 17:25:25
+ * Author: mcxiaoke (github@mcxiaoke.com)
+ * License: Apache License 2.0
+ */
+import path from 'path'
+import * as log from '../lib/debug.js'
+import * as mf from '../lib/file.js'
+import * as helper from '../lib/helper.js'
+
+/**
+ * 计算每个目录下的文件路径列表
+ * @param {string[]} filePaths - 文件绝对路径列表
+ * @returns {Map} - fileListMap: Map<string, string[]>
+ */
+function countFilesInDirectories(filePaths) {
+    const fileListMap = new Map()
+
+    filePaths.forEach(filePath => {
+        const dirname = path.dirname(filePath)
+        const basename = path.basename(filePath)
+        // 判断是否有扩展名
+        const isFile = path.extname(basename) !== ''
+        if (isFile) {
+            // 更新文件列表Map
+            if (fileListMap.has(dirname)) {
+                fileListMap.get(dirname).push(filePath)
+            } else {
+                fileListMap.set(dirname, [filePath])
+            }
+        }
+    })
+
+    return fileListMap
+}
+
+async function rmFiles() {
+    const root = process.argv[2]
+    const walkOpts = {
+        needStats: false,
+        entryFilter: (f) => helper.isImageFile(f.path)
+    }
+    log.showGreen('rmFiles', `Walking files ...`)
+    let entries = await mf.walk(root, walkOpts)
+    let filePaths = entries.map(e => e.path)
+    let map = countFilesInDirectories(filePaths)
+    for (const [key, value] of map) {
+        log.show(`Found [${value.length}] files in ${key}`)
+        if (value.length > 800) {
+            let i = 0
+            for (const f of value) {
+                // 删除1/4文件
+                if (++i % 4 === 0) {
+                    await helper.safeRemove(f)
+                }
+            }
+            if (i > 0) {
+                log.showGreen(`Deleted [${Math.floor(i / 4)}/${value.length}] files in ${key}`)
+            }
+        }
+    }
+
+}
+
+await rmFiles()
